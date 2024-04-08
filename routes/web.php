ae2e0bd99b4d00d2aff2bcd9f43c31af22a503d3
Demix357 <Demix357@yandex.ru>
2024-04-08 22:00:25 +0800

    change Knowledge Core

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,15 @@ Route::middleware(['set_locale'])->group(function() {
     Route::group(['prefix' => 'admin'], function() {
         Route::get('/', [IndexController::class, 'index'])->name('admin.index');
         Route::name('admin.')->group(function () {
-            Route::resource('knowledge-core', KnowledgeCoreController::class);
+            //Route::resource('knowledge-core', KnowledgeCoreController::class);
+            Route::group(['prefix' => 'knowledge-core'], function() {
+                Route::get('/', [KnowledgeCoreController::class, 'index'])->name('knowledge-core.index');
+                Route::get('/create', [KnowledgeCoreController::class, 'create'])->name('knowledge-core.create');
+                Route::post('/', [KnowledgeCoreController::class, 'store'])->name('knowledge-core.store');
+                Route::get('/{knowledge_core}/edit', [KnowledgeCoreController::class, 'edit'])->name('knowledge-core.edit');
+                Route::patch('/{knowledge_core}', [KnowledgeCoreController::class, 'update'])->name('knowledge-core.update');
+                Route::delete('/{knowledge_core}', [KnowledgeCoreController::class, 'destroy'])->name('knowledge-core.destroy');
+            });
             Route::resource('news', NewsController::class);
             Route::resource('developments', DevelopmentsController::class);
             Route::resource('project', ProjectController::class);
