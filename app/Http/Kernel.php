936af6da37c04d85478df7fa9738d1bff70ec43d
LedZeppe1in <tualatin32@mail.ru>
2024-04-23 14:32:32 +0800

    Added CORS in middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'set_locale' => \App\Http\Middleware\SetLocale::class
+        'set_locale' => \App\Http\Middleware\SetLocale::class,
+        'cors' => \App\Http\Middleware\Cors::class
     ];
 }
