5fa314fcfa957e777045f7876c611a70f32b3247
LedZeppe1in <tualatin32@mail.ru>
2025-09-23 11:11:24 +0800

    Updated user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -24,5 +24,19 @@ class UserSeeder extends Seeder
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
+
+        // Создание пользователя по умолчанию
+        DB::table('users')->insert([
+            'name' => 'demo',
+            'email' => 'demo@knowledge-core.ru',
+            'password' => bcrypt('demo'),
+            'role' => User::USER_ROLE,
+            'status' => User::ACTIVE_STATUS,
+            'full_name' => 'Тестовый пользователь',
+            'last_login_date' => Carbon::now(),
+            'login_ip' => '127.0.0.1',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
