44ec77455944b04c9b58686afecbc5676891fb8e
LedZeppe1in <tualatin32@mail.ru>
2025-09-23 10:11:06 +0800

    Minor fix

--- database/seeders/KnowledgeCoreSeeder.php
+++ database/seeders/KnowledgeCoreSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\KnowledgeCore;
 use Carbon\Carbon;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -15,15 +13,15 @@ class KnowledgeCoreSeeder extends Seeder
      */
     public function run(): void
     {
-        // Создание администратора по умолчанию
+        // Создание основной информации о проекте по умолчанию
         DB::table('knowledge_core')->insert([
             'description' => 'Тестовое описание',
-            'phone' => 'Указать телефон',
+            'phone' => 'Телефона нет',
             'email' => 'admin@knowledge-core.ru',
-            'address' => 'Указать адрес',
-            'references' => 'Указать публикации',
-            'lab_link' => 'Указать ссылку',
-            'github_link' => 'Указать ссылку',
+            'address' => 'Адреса нет',
+            'references' => 'Публикаций нет',
+            'lab_link' => 'http://www.safety-irk.ru/',
+            'github_link' => 'https://github.com/Lab42-Team/',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
